cd14da1caaf4d362f797030aed062a5885a66132
Ramil Safin <RamilSafNab1996@gmail.com>
2021-02-23 01:18:43 +0300

    Update tests

--- tests/tasks_tests.cpp
+++ tests/tasks_tests.cpp
@@ -4,17 +4,4 @@
 
 SCENARIO("Demo") {
 
-    GIVEN("Pointers to local variables") {
-        WHEN("1") {
-            THEN("2") {
-                int a = 0;
-                int b = 5;
-
-                swap_args(&a, &b);
-
-                CHECK(a == 5);
-                CHECK(b == 0);
-            }
-        }
-    }
 }
